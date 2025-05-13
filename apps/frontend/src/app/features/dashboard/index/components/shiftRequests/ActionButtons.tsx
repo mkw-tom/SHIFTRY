f37bb778f23622e40fb193725236ae1de72b5c86
Tomu Maekawa <tomu.m1107@icloud.com>
2025-05-13 12:07:27 +0900

    creat bottom drawer

---
+++ apps/frontend/src/app/features/dashboard/index/components/shiftRequests/ActionButtons.tsx
@@ -0,0 +1,77 @@
+"use client";
+import type { RequestStatus } from "@prisma/client";
+import type { ShiftRequest } from "@shared/common/types/prisma";
+import {
+	DrawerView,
+	useBottomDrawer,
+} from "../../../common/context/useBottomDrawer";
+
+const ActionButtons = ({
+	status,
+	data,
+}: {
+	status: RequestStatus;
+	data: ShiftRequest;
+}) => {
+	const { darawerOpen } = useBottomDrawer();
+
+	switch (status) {
+		case "HOLD":
+			return (
+				<button
+					type="button"
+					className="w-28 h-7 rounded-full bg-gray02 text-xs text-white font-bold shadow-sm"
+					onClick={() => darawerOpen(DrawerView.CREATE_REQUEST, data)}
+				>
+					作成再開
+				</button>
+			);
+
+		case "REQUEST":
+			return (
+				<>
+					<button
+						type="button"
+						className="w-28 h-7 rounded-full bg-green03 text-xs text-green02 font-bold shadow-sm"
+						onClick={() => darawerOpen(DrawerView.STATUS, data)}
+					>
+						提出確認・回収
+					</button>
+					<button
+						type="button"
+						className="w-28 h-7 rounded-full bg-green02 text-xs text-white font-bold shadow-sm"
+						onClick={() => darawerOpen(DrawerView.SUBMIT, data)}
+					>
+						シフト提出
+					</button>
+				</>
+			);
+
+		case "ADJUSTMENT":
+			return (
+				<button
+					type="button"
+					className="w-28 h-7 rounded-full bg-blue01 text-xs text-white font-bold shadow-sm"
+					onClick={() => darawerOpen(DrawerView.ADJUSTMENT, data)}
+				>
+					調整を見る
+				</button>
+			);
+
+		case "CONFIRMED":
+			return (
+				<button
+					type="button"
+					className="w-28 h-7 rounded-full bg-orange-400 text-xs text-white font-bold shadow-sm"
+					onClick={() => darawerOpen(DrawerView.CONFIRM, data)}
+				>
+					完成確認
+				</button>
+			);
+
+		default:
+			return null;
+	}
+};
+
+export default ActionButtons;
