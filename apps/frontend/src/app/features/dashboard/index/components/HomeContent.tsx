5803442931fcc2e002bae48c1edeb8f18c772c0f
Tomu Maekawa <tomu.m1107@icloud.com>
2025-05-18 21:10:13 +0900

    created UI and fetch API

--- apps/frontend/src/app/features/dashboard/index/components/HomeContent.tsx
+++ apps/frontend/src/app/features/dashboard/index/components/HomeContent.tsx
@@ -1,11 +1,58 @@
 "use client";
-import React, { useState } from "react";
+import { useNavigation } from "@/app/lib/navigation";
+import type { RootState } from "@/app/redux/store";
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useAutoLogin } from "../../common/hook/useAutoLogin";
+import AutoLoginError from "./AutoLoginError";
+import AutoLoginLoading from "./AutoLoginLoading";
 import HeadSwitch from "./HeadSwitch";
 import MembersContent from "./members/MembersContent";
 import ShiftRequestList from "./shiftRequests/ShiftRequestList";
 
 const HomeContent = () => {
 	const [select, setSelect] = useState<"SHIFT" | "MEMBER">("SHIFT");
+	const { userToken, storeToken, groupToken } = useSelector(
+		(state: RootState) => state.token,
+	);
+	const { user } = useSelector((state: RootState) => state.user);
+	const { store } = useSelector((state: RootState) => state.store);
+	const { stores } = useSelector((state: RootState) => state.stores);
+	const { handleAutoLogin, error, isLoading, setError } = useAutoLogin();
+	const hasRun = useRef(false);
+
+	useEffect(() => {
+		const autoLoginFunc = async () => {
+			const isLoggedIn = !!user?.id && !!store?.id && stores.length > 0;
+			const hasTokens = !!userToken && !!storeToken && !!groupToken;
+
+			if (isLoggedIn) return;
+			if (hasRun.current || !hasTokens) {
+				setError(true);
+				return;
+			}
+
+			hasRun.current = true;
+			await handleAutoLogin({ userToken: userToken, storeToken, groupToken });
+		};
+		autoLoginFunc();
+	}, [
+		user,
+		store,
+		stores,
+		userToken,
+		storeToken,
+		groupToken,
+		setError,
+		handleAutoLogin,
+	]);
+
+	if (error) {
+		return <AutoLoginError />;
+	}
+	if (isLoading) {
+		return <AutoLoginLoading />;
+	}
 
 	return (
 		<div className="w-full h-auto">
