10bb861d3b4adb08953e26d6fc464626622c3467
Tomu Maekawa <tomu.m1107@icloud.com>
2025-05-18 22:06:12 +0900

    fix detail

--- apps/frontend/src/app/features/auth/login/components/LoginHead.tsx
+++ apps/frontend/src/app/features/auth/login/components/LoginHead.tsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 const LoginHead = () => {
 	const { stores } = useSelector((state: RootState) => state.stores);
 
-	if (stores.length >= 1) {
+	if (stores.length > 1) {
 		return (
 			<h2 className="text-center font-bold text-sm text-black border-b-1 border-b-gray01 pb-1">
 				ログインする店舗を選択
