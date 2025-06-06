d6246d920e6a424e0c8fe439a165f38bdb90cf1b
Tomu Maekawa <tomu.m1107@icloud.com>
2025-06-06 23:26:14 +0900

    fix api error of empty name

--- apps/frontend/src/app/features/dashboard/common/components/bottomDrawer/submit/Submit.tsx
+++ apps/frontend/src/app/features/dashboard/common/components/bottomDrawer/submit/Submit.tsx
@@ -25,7 +25,8 @@ const Submit = () => {
 	const { currentData } = useBottomDrawer();
 	const { userToken, storeToken } = useSelector(
 		(state: RootState) => state.token,
-	);
+	)
+	const { user } = useSelector((state: RootState) => state.user)
 	const { handleGetSubmitShiftUserOne, isLoading: getDataLoading } =
 		useGetSubmittedShiftUserOne();
 	const { handleUpsertSubmitShift, isLoading: submitLoading } =
@@ -69,7 +70,7 @@ const Submit = () => {
 		shiftRequestId: "",
 		status: ShiftStatus.ADJUSTMENT,
 		shifts: {
-			name: "",
+			name: user?.name as string ,
 			weekCountMax: 0,
 			weekCountMin: 0,
 			availableWeeks: [],
